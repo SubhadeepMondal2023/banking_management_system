91e8fcdc5f8564f8911afceadc3ad6a174bc9848
Subha <msubhadeep9635@gmail.com>
2025-09-04 19:13:03 +0530

    added jwt and cors config

--- banking/src/main/java/com/project/banking/entity/User.java
+++ banking/src/main/java/com/project/banking/entity/User.java
@@ -1,11 +1,16 @@
 package com.project.banking.entity;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -18,7 +23,7 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "bank_user")
-public class User {
+public class User implements UserDetails{
 
     /**
      * Unique identifier for the user.
@@ -80,5 +85,15 @@ public class User {
     @OneToMany(mappedBy = "owner", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @JsonIgnore
     private List<Account> accounts;
+
+    /**
+     * Returns a collection of granted authorities based on the user's roles.
+     *
+     * @return Collection of granted authorities.
+     */
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+    }
 
 }
